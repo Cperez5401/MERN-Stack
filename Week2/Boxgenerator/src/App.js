70a83e332f25647d5789b30d38bc3a69fefd62aa
Cperez5401 <80223406+Cperez5401@users.noreply.github.com>
2021-07-09 13:31:20 -0700

    Add files via upload

---
+++ Week2/Boxgenerator/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BoxGenerator from './components/boxGenerator'
+
+function App() {
+  return (
+    <div className="App">
+      <BoxGenerator />
+    </div>
+  );
+}
+
+export default App;
